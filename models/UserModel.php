fdcb200c30eaa9be593360a1f2266b3eda6c2fc1
Pavle <pavlest1906@gmail.com>
2024-11-12 22:46:51 +0100

    Added db queries and validation

--- models/UserModel.php
+++ models/UserModel.php
@@ -6,16 +6,32 @@ use app\core\BaseModel;
 
 class UserModel extends BaseModel
 {
-    public string $email;
-    public string $firstName;
-    public string $lastName;
+    public int $id;
+    public string $email = '';
+    public string $first_name = '';
+    public string $last_name = '';
 
-    public function __construct()
-    {
-    }
-
-    public function tableName()
+    public function tableName(): string
     {
         return "users";
+    }
+
+    public function readColumns(): array
+    {
+        return ["id", "email", "first_name", "last_name"];
+    }
+
+    public function editColumns(): array
+    {
+        return ["email", "first_name", "last_name"];
+    }
+
+    public function validationRules(): array
+    {
+        return [
+            "email" => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            "first_name" => [self::RULE_REQUIRED],
+            "last_name" => [self::RULE_REQUIRED],
+        ];
     }
 }
